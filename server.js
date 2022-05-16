eecd5e8804c89fd87f2fc6e4d4cab665ace847bd
Gabriel <gmamanican@unsa.edu.pe>
2022-05-16 09:37:20 -0500

    using express and listen port

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const app = express();
 const cors = require("cors");
 var MarkdownIt = require("markdown-it"),
   md = new MarkdownIt();
+
+app.use(express.static("pub"));
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Escuchando en: http://localhost:3000");
+});
